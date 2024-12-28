a37341dd8a4f581ce8d01b37f3ba0810c46c8ef9
pranayasahu <pranaya_sahu@live.com>
2024-12-28 21:42:43 +0530

    Solution to get 2 integer from input array whose sum is equal to input targetSum.

---
+++ Algo/src/com/pranaya/AlgoChala/ArraysSolution.java
@@ -0,0 +1,47 @@
+package com.algochala.ArraysExample;
+
+import java.util.Arrays;
+
+public class ArraysSolution {
+    public static void main(String[] args) {
+        int[] array = new int[]{3, 5, -4, 2, 11, 1, -1, 61};
+        int targetSum = 10;
+        System.out.println(Arrays.toString(twoNumberSum_solution2(array, targetSum)));
+    }
+
+    public static int[] twoNumberSum_solution2(int[] array, int targetSum) {
+        Arrays.sort(array);
+        int firstIndex = 0;
+        int secondIndex = array.length - 1;
+        int[] solution = new int[0];
+        while(firstIndex < secondIndex){
+            if((array[firstIndex]+array[secondIndex]) == targetSum){
+                solution = new int[2];
+                solution[0] = array[firstIndex];
+                solution[1] = array[secondIndex];
+                return solution;
+            }else if((array[firstIndex]+array[secondIndex]) > targetSum){
+                secondIndex--;
+            }else {
+                firstIndex++;
+                secondIndex = array.length-1;
+            }
+        }
+        return solution;
+    }
+
+    public static int[] twoNumberSum_solution1(int[] array, int targetSum) {
+        int[] solution = new int[0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if ((array[i] + array[j]) == targetSum) {
+                    solution = new int[2];
+                    solution[0] = array[i];
+                    solution[1] = array[j];
+                    return solution;
+                }
+            }
+        }
+        return solution;
+    }
+}
