fe1ba603179855815c1abd4810bbdb9ff2d80bb5
pranayasahu <pranaya_sahu@live.com>
2025-02-08 13:31:33 +0530

    New logic added for SortedSquaredArray.

--- Algo/src/com/pranaya/AlgoChala/SortedSquaredArray.java
+++ Algo/src/com/pranaya/AlgoChala/SortedSquaredArray.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class SortedSquaredArray {
     public static void main(String[] args) {
         //array = [1, 2, 3, 5, 6, 8, 9]
-        int[] array = new int[]{-10, 1, 2, 3, 5, 6, 8, 9};
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.println(Arrays.toString(sortedSquaredArray(array)));
     }
 
-    private static int[] sortedSquaredArray(int[] array) {
+    /*private static int[] sortedSquaredArray(int[] array) {
         return Arrays.stream(array)
                 .map(operand -> {
                     if (operand < 0) operand = operand * -1;
@@ -18,5 +18,35 @@ public class SortedSquaredArray {
                 .sorted()
                 .map(value -> value * value)
                 .toArray();
+    }*/
+
+    /*
+    Time O(1) and space O(1)
+     */
+    private static int[] sortedSquaredArray(int[] array) {
+        int pivot = getFirstPositivePosition(array);
+        int left = pivot - 1, right = pivot;
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            int leftValue = left < 0 ? Integer.MAX_VALUE : array[left] * array[left];
+            int rightValue = right >= array.length ? Integer.MAX_VALUE : array[right] * array[right];
+            if (leftValue >= rightValue) {
+                result[i] = rightValue;
+                right++;
+            } else {
+                result[i] = leftValue;
+                left--;
+            }
+        }
+        return result;
+    }
+
+    private static int getFirstPositivePosition(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                return i;
+            }
+        }
+        return array.length - 1;
     }
 }
